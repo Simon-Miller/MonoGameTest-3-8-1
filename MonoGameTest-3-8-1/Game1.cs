fc4ff9fec752339b10a84870feaaafc1a39b7dd6
Simon Miller <simonmiller17@hotmail.co.uk>
2023-02-25 08:06:25 +0000

    optimised a little, and made the 'mg' sprite controllable by the cursor keys

--- MonoGameTest-3-8-1/Game1.cs
+++ MonoGameTest-3-8-1/Game1.cs
@@ -7,52 +7,64 @@ namespace MonoGameTest_3_8_1
 {
     public class Game1 : Game
     {
+        // ---------------------------------
         const int wide = 640;
         const int high = 480;
         const int numPixels = wide * high;
+        const bool isFullScreen = false;
+        // ---------------------------------
 
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
             // TURN OFF FULL SCREEN by commenting out, or setting to false.
-            //_graphics.IsFullScreen = true;
-            _graphics.PreferredBackBufferWidth = 640;
-            _graphics.PreferredBackBufferHeight = 480;
+            graphics.IsFullScreen = isFullScreen;
+            graphics.PreferredBackBufferWidth = 640;
+            graphics.PreferredBackBufferHeight = 480;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
             var currentPath = System.IO.Directory.GetCurrentDirectory();
-            this.sprite = Texture2D.FromFile(_graphics.GraphicsDevice, "..\\..\\..\\Icon.bmp");
+            this.sprite = Texture2D.FromFile(graphics.GraphicsDevice, "..\\..\\..\\Icon.bmp");
 
-            this.screen = new Texture2D(_graphics.GraphicsDevice, wide, high);
+            this.screen = new Texture2D(graphics.GraphicsDevice, wide, high);
 
             base.Initialize();
         }
 
-        Texture2D sprite;
         Texture2D screen;
+        Vector2 screenPosition = new Vector2(0, 0);
+
+        Texture2D sprite;
+        Vector2 spritePosition = new Vector2(100, 100);
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            var keyboardState = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            if (keyboardState.IsKeyDown(Keys.Up)) spritePosition.Y--;
+            if (keyboardState.IsKeyDown(Keys.Down)) spritePosition.Y++;
+            if (keyboardState.IsKeyDown(Keys.Left)) spritePosition.X--;
+            if (keyboardState.IsKeyDown(Keys.Right)) spritePosition.X++;
 
             base.Update(gameTime);
         }
@@ -60,10 +72,11 @@ namespace MonoGameTest_3_8_1
         
         UInt32[] screenData = new UInt32[numPixels];
         Random random = new Random();
+        
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            //GraphicsDevice.Clear(Color.CornflowerBlue);
 
             for (var i = 0; i < numPixels; i++)
                 screenData[i] = ((uint)(random.Next(0xffffff)) | 0xff000000);
@@ -71,10 +84,10 @@ namespace MonoGameTest_3_8_1
             screen.SetData<UInt32>(screenData);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(screen, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(sprite, new Vector2(100,100), Color.White);
-            _spriteBatch.End();
+            spriteBatch.Begin();
+            spriteBatch.Draw(screen, screenPosition, Color.White);
+            spriteBatch.Draw(sprite, spritePosition, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
