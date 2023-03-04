17f227665806f87b322f7250309356a66dbf58cf
Guildor <simonmiller17@hotmail.co.uk>
2023-03-04 22:26:25 +0000

    local commit

--- MonoGameTest-3-8-1/Game1.cs
+++ MonoGameTest-3-8-1/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.IO;
 
 namespace MonoGameTest_3_8_1
 {
@@ -12,6 +13,7 @@ namespace MonoGameTest_3_8_1
         const int high = 480;
         const int numPixels = wide * high;
         const bool isFullScreen = false;
+        const int BALLS = 10;
         // ---------------------------------
 
         private GraphicsDeviceManager graphics;
@@ -37,6 +39,11 @@ namespace MonoGameTest_3_8_1
 
             this.screen = new Texture2D(graphics.GraphicsDevice, wide, high);
 
+            this.ballSprite = Texture2D.FromFile(graphics.GraphicsDevice, "..\\..\\..\\cool-ball.png");
+            
+
+            this.setupBalls();
+
             base.Initialize();
         }
 
@@ -45,6 +52,8 @@ namespace MonoGameTest_3_8_1
 
         Texture2D sprite;
         Vector2 spritePosition = new Vector2(100, 100);
+
+        Texture2D ballSprite;
 
         protected override void LoadContent()
         {
@@ -66,30 +75,97 @@ namespace MonoGameTest_3_8_1
             if (keyboardState.IsKeyDown(Keys.Left)) spritePosition.X--;
             if (keyboardState.IsKeyDown(Keys.Right)) spritePosition.X++;
 
+            // update balls.
+            for (int i = 0; i < balls.Length; i++)
+                updateBallPosition(balls[i]);
+
             base.Update(gameTime);
         }
 
-        
-        UInt32[] screenData = new UInt32[numPixels];
+
         Random random = new Random();
-        
+
+        Ball[] balls = new Ball[BALLS];
+
 
         protected override void Draw(GameTime gameTime)
         {
-            //GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            for (var i = 0; i < numPixels; i++)
-                screenData[i] = ((uint)(random.Next(0xffffff)) | 0xff000000);
-
-            screen.SetData<UInt32>(screenData);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            spriteBatch.Draw(screen, screenPosition, Color.White);
+
+            
+            for (int i = 0; i < balls.Length; i++)
+                spriteBatch.Draw(ballSprite, balls[i].Position, Color.White);
+
             spriteBatch.Draw(sprite, spritePosition, Color.White);
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        private void setupBalls()
+        {
+            for (int i = 0; i < balls.Length; i++)
+            {
+                var position = new Vector2(random.Next(wide - 32), random.Next(high -32));
+                var directionX = (sbyte)(((sbyte)random.Next(1) * 2) - 1);
+                var directionY = (sbyte)(((sbyte)random.Next(1) * 2) - 1);
+                var xSpeed = random.Next(10) + 1;
+                var ySpeed = random.Next(10) + 1;
+
+                balls[i] = new Ball()
+                {
+                    Position = position,
+                    DirectionX = directionX,
+                    DirectionY = directionY,
+                    XSpeed = xSpeed,
+                    YSpeed = ySpeed,
+                };
+            }
+        }
+
+        const int maxX = wide - 32;
+        const int maxY = high - 32;
+        private void updateBallPosition(Ball ball)
+        {
+            if(ball.DirectionY == -1 && ball.Position.Y < 0)
+                ball.DirectionY = 1;
+            
+            if(ball.DirectionY == 1 && ball.Position.Y >= maxY)
+                ball.DirectionY = -1;
+            
+            if(ball.DirectionX == -1 && ball.Position.X < 0)
+                ball.DirectionX = 1;
+            
+            if(ball.DirectionX == 1 && ball.Position.X >= maxX)
+                ball.DirectionX = -1;
+
+            float newX = ball.Position.X;
+            float newY = ball.Position.Y;
+
+            if (ball.DirectionX == 1)
+                newX += ball.XSpeed;
+            else
+                newX -= ball.XSpeed;
+
+            if (ball.DirectionY == 1)
+                newY += ball.YSpeed;
+            else
+                newY -= ball.YSpeed;
+
+            ball.Position = new Vector2(newX, newY);
+        }
+    }
+
+    class Ball
+    {
+        public Vector2 Position;
+        public sbyte DirectionX;
+        public sbyte DirectionY;
+        public int XSpeed;
+        public int YSpeed;
     }
 }
