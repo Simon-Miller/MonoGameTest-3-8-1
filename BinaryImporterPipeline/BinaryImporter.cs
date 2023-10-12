fa34393d58dd6d9928e62fc7089819ec5c2e9a06
Simon Miller <simonmiller17@hotmail.co.uk>
2023-10-12 13:59:16 +0100

    Added BinaryImporter pipeline for DesktopGL to the project

---
+++ BinaryImporterPipeline/BinaryImporter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System;
+
+namespace BinaryImporterPipeline
+{
+    /// <summary>
+    /// This class will be instantiated by the XNA Framework Content Pipeline
+    /// to import a file from disk into the specified type, TImport.
+    ///
+    /// This should be part of a Content Pipeline Extension Library project.
+    ///
+    /// TODO: change the ContentImporter attribute to specify the correct file
+    /// extension, display name, and default processor for this importer.
+    /// </summary>
+
+    [ContentImporter(".bin", DisplayName = "Binary Importer", DefaultProcessor = nameof(BinaryProcessor))]
+    public class BinaryImporter : ContentImporter<byte[]>
+    {
+        public override byte[] Import(string filename, ContentImporterContext context)
+        {
+            return System.IO.File.ReadAllBytes(filename);
+        }
+    }
+}
---
+++ BinaryImporterPipeline/BinaryProcessor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace BinaryImporterPipeline
+{
+    /// <summary>
+    /// This class will be instantiated by the XNA Framework Content Pipeline
+    /// to apply custom processing to content data, converting an object of
+    /// type TInput to TOutput. The input and output types may be the same if
+    /// the processor wishes to alter data without changing its type.
+    ///
+    /// This should be part of a Content Pipeline Extension Library project.
+    ///
+    /// TODO: change the ContentProcessor attribute to specify the correct
+    /// display name for this processor.
+    /// </summary>
+    [ContentProcessor(DisplayName = "Binary Processor")]
+    public class BinaryProcessor : ContentProcessor<byte[], byte[]>
+    {
+        public override byte[] Process(byte[] input, ContentProcessorContext context)
+        {
+            return input;
+        }
+    }
+
+    /// <summary>
+    /// Pipeline will use this to process the internal byte[] (maybe we'll have it compressed in future?)
+    /// To the output un-compressed (not that it's currently compressed, by MonoGame doesn't know that)
+    /// </summary>    
+    public class BinaryReader : ContentTypeReader<byte[]>
+    {
+        protected override byte[] Read(ContentReader input, byte[] existingInstance)
+        {
+            byte[] rawData = input.ReadBytes((int)input.BaseStream.Length);
+
+            return rawData;
+        }
+    }
+}
